1c467b82725e73e1834cb282d9c0c1873c549a5e
Jeffred Lim <58154782+jepjepj@users.noreply.github.com>
2025-04-16 22:04:44 +0800

    Update env.php

--- core/env.php
+++ core/env.php
@@ -5,13 +5,13 @@ define('BASE_URL', 'https://qlpascorp.com/' . APP_FOLDER . '/');
 define("BASE_PATH", __DIR__ . "/../");
 define('DEVELOPMENT', true);
 
-// define("HOST", "localhost");
-// define("USER", "root");
-// define("PASSWORD", "");
-// define("DBNAME", "jcis_acc_db");
-
-
 define("HOST", "localhost");
-define("USER", "jcisaccountingroot");
-define("PASSWORD", "DRkpSpM9MRSULEdKfdUz");
-define("DBNAME", "jcisaccountingdb");
+define("USER", "u814036432_accounting");
+define("PASSWORD", "6Gs]Fq/]n");
+define("DBNAME", "u814036432_accounting_db");
+
+
+// define("HOST", "localhost");
+// define("USER", "jcisaccountingroot");
+// define("PASSWORD", "DRkpSpM9MRSULEdKfdUz");
+// define("DBNAME", "jcisaccountingdb");
