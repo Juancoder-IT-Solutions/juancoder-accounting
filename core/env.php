3d6f7c10897414e4434e21ad91dfc90e56456c21
Jeffred Lim <58154782+jepjepj@users.noreply.github.com>
2025-07-14 11:28:57 +0800

    new vps config

--- core/env.php
+++ core/env.php
@@ -5,11 +5,15 @@ define('BASE_URL', 'https://qlpascorp.com/' . APP_FOLDER . '/');
 define("BASE_PATH", __DIR__ . "/../");
 define('DEVELOPMENT', true);
 
-define("HOST", "localhost");
-define("USER", "u814036432_accounting");
-define("PASSWORD", "6Gs]Fq/]n");
-define("DBNAME", "u814036432_accounting_db");
+// define("HOST", "localhost");
+// define("USER", "u814036432_accounting");
+// define("PASSWORD", "6Gs]Fq/]n");
+// define("DBNAME", "u814036432_accounting_db");
 
+define("HOST", "localhost");
+define("USER", "juancoder_accounting_root");
+define("PASSWORD", "NdHH9GT6p32sXqsK");
+define("DBNAME", "jcis_accounting_db");
 
 // define("HOST", "localhost");
 // define("USER", "jcisaccountingroot");
